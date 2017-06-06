e04163f593272537f226bf2ddb17c640a6a81960
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2017-06-06 16:51:25 +0200

    Retry and better error messages (#8)
    * Add a VaultError type
    * Add/clarify error messages for vault responses
    * Some code formatting and moving
    * Implement retries on 5xx responses and exceptions
    * Quick and dirty integration test automation
    * Use lens-aeson instead of json-stream

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Control.Concurrent.Async as Async
-import           Data.Char
-import           Data.List (findIndex, nubBy)
-import qualified Data.ByteString.Char8 as SBS
-import qualified Data.ByteString.Lazy as LBS
-import           Data.JsonStream.Parser hiding (value)
-import           Data.Text (Text)
-import qualified Data.Text as Text
-import           Data.Semigroup ((<>))
-import           Network.HTTP.Simple
-import           Options.Applicative hiding (Parser, command)
-import qualified Options.Applicative as O
-import           System.Environment
-import           System.Posix.Process
+import Control.Concurrent   (threadDelay)
+import Control.Lens         (preview)
+import Data.Char
+import Data.List            (findIndex, nubBy)
+import Data.Semigroup       ((<>))
+import Network.HTTP.Simple
+import Options.Applicative  hiding (Parser, command)
+import System.Environment
+import System.Posix.Process
+import System.Random        (getStdRandom, randomR)
+
+import qualified Control.Concurrent.Async   as Async
+import qualified Control.Exception          as Exception
+import qualified Data.Aeson.Lens            as Lens (key, _String)
+import qualified Data.ByteString.Char8      as SBS
+import qualified Data.ByteString.Lazy       as LBS hiding (unpack)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Text                  as Text
+import qualified Options.Applicative        as O
 
 --
 -- Datatypes
@@ -35,6 +40,16 @@ data Secret = Secret
   } deriving (Eq, Show)
 
 type EnvVar = (String, String)
+
+data VaultError
+  = SecretNotFound    Secret
+  | KeyNotFound       Secret
+  | BadRequest        LBS.ByteString
+  | Forbidden
+  | ServerError       LBS.ByteString
+  | ServerUnavailable LBS.ByteString
+  | ServerUnreachable
+  | Unspecified       Int LBS.ByteString
 
 --
 -- Argument parsing
@@ -91,11 +106,10 @@ main = do
       Right ss -> ss
       Left err -> errorWithoutStackTrace err
 
-  responses <- Async.mapConcurrently (requestSecret opts) secrets
+  newEnvOrErrors <- Async.mapConcurrently (requestSecret opts) secrets
 
   let
-    newEnvOrError = mapM (uncurry parseResponse) responses
-    newEnv = case newEnvOrError of
+    newEnv = case sequence newEnvOrErrors of
       -- We need to eliminate duplicates in the environment and keep
       -- the first occurrence. `nubBy` (from Data.List) runs in O(n^2),
       -- but this shouldn't matter for our small lists.
@@ -103,17 +117,10 @@ main = do
       -- Equality is determined on the first element of the env var
       -- tuples.
       Right e -> nubBy (\(a,_) (b,_) -> a == b) (e ++ env)
-      Left err -> errorWithoutStackTrace err
+      Left err -> errorWithoutStackTrace (vaultErrorLogMessage err)
 
   runCommand opts newEnv
 
--- Like splitAt, but also removes the character at the split position.
-cutAt :: Int -> [a] -> ([a], [a])
-cutAt index xs =
-  let
-    (first, second) = splitAt index xs
-  in
-    (first, drop 1 second)
 
 parseSecret :: String -> Either String Secret
 parseSecret line =
@@ -134,8 +141,10 @@ parseSecret line =
                 , sVarName = varName
                 }
 
+
 readSecretList :: FilePath -> IO (Either String [Secret])
 readSecretList fname = fmap (sequence . fmap parseSecret . lines) (readFile fname)
+
 
 runCommand :: Options -> [EnvVar] -> IO a
 runCommand options env =
@@ -150,43 +159,111 @@ runCommand options env =
     executeFile command searchPath args env'
 
 
-requestSecret :: Options -> Secret -> IO (Secret, Response LBS.ByteString)
-requestSecret opts secret = do
-  let request = setRequestHeader "x-vault-token" [SBS.pack (oVaultToken opts)]
-              $ setRequestPath (requestPath secret)
-              $ setRequestPort (oVaultPort opts)
-              $ setRequestHost (SBS.pack (oVaultHost opts))
-              $ defaultRequest
+requestSecret :: Options -> Secret -> IO (Either VaultError EnvVar)
+requestSecret opts secret =
+  let
+    requestPath = "/v1/secret/" <> (sPath secret)
+    request = setRequestHeader "x-vault-token" [SBS.pack (oVaultToken opts)]
+            $ setRequestPath (SBS.pack requestPath)
+            $ setRequestPort (oVaultPort opts)
+            $ setRequestHost (SBS.pack (oVaultHost opts))
+            $ defaultRequest
 
-  response <- httpLBS request
-  pure (secret, response)
+  in do
+    responseOrErr <- httpWithRetry 3 request
+    case responseOrErr of
+      (Left _) -> pure $ Left ServerUnreachable
+      (Right response) -> pure $ parseResponse secret response
 
-  where
-    requestPath :: Secret -> SBS.ByteString
-    requestPath s = "/v1/secret/" `SBS.append` (SBS.pack (sPath s))
+
+httpWithRetry :: Int -> Request -> IO (Either (HttpException) (Response LBS.ByteString))
+httpWithRetry 1 request = Exception.try $ httpLBS request
+httpWithRetry triesLeft request =
+  let
+    retry = do
+      -- Get a random jitter between 0 and 100 ms (inclusive).
+      jitter <- getStdRandom (randomR (0, 100))
+      let
+        delayMilliseconds = 50 + jitter
+        delayMicroseconds = 1000 * delayMilliseconds
+      -- Wait a bit before retrying, at least 50 ms, at most 150 ms.
+      -- TODO: Exponential backoff.
+      threadDelay delayMicroseconds
+      httpWithRetry (triesLeft - 1) request
+
+  in do
+    responseOrError <- Exception.try $ httpLBS request
+    case responseOrError of
+      Right response ->
+        case (getResponseStatusCode response) of
+          500 -> retry -- Internal Server Error
+          503 -> retry -- Service Unavailable
+          504 -> retry -- Gateway Timeout
+          _   -> pure responseOrError
+      Left (HttpExceptionRequest _ _) -> retry
+      Left a -> Exception.throw a
+
 
 --
 -- HTTP response handling
 --
 
-parseResponse :: Secret -> Response LBS.ByteString -> Either String EnvVar
+parseResponse :: Secret -> Response LBS.ByteString -> Either VaultError EnvVar
 parseResponse secret response =
-  case (getResponseStatusCode response) of
-    403 -> Left $ "[ERROR] Vault token is invalid"
-    404 -> Left $ "[ERROR] Secret not found: " ++ path
-    _   -> fmap (\c -> (sVarName secret, Text.unpack c)) content
-  where
-    body = getResponseBody response
-    key = sKey secret
-    path = sPath secret
-    at = "data" .:? (Text.pack key) .: string
-    getContent json = parseLazyByteString at json :: [Maybe Text]
-    -- This call to head should never fail: we will always get a response with
-    -- a "data" key from Vault
-    content = maybeToEither ("[ERROR] Key '" ++ key ++ "' not found in: " ++ path) $ head (getContent body)
+  let
+    responseBody = getResponseBody response
+    statusCode = getResponseStatusCode response
+  in case statusCode of
+    200 -> parseSuccessResponse secret responseBody
+    403 -> Left Forbidden
+    404 -> Left $ SecretNotFound secret
+    500 -> Left $ ServerError responseBody
+    503 -> Left $ ServerUnavailable responseBody
+    _   -> Left $ Unspecified statusCode responseBody
 
 
--- Converts a secret into the name of the environment variable
+parseSuccessResponse :: Secret -> LBS.ByteString -> Either VaultError EnvVar
+parseSuccessResponse secret responseBody =
+  let
+    secretKey = Text.pack (sKey secret)
+    getter = Lens.key "data" . Lens.key secretKey . Lens._String
+    toEnvVar secretValue = (sVarName secret, Text.unpack secretValue)
+  in
+    maybeToEither (KeyNotFound secret) $ fmap toEnvVar (preview getter responseBody)
+
+--
+-- Utility functions
+--
+
+vaultErrorLogMessage :: VaultError -> String
+vaultErrorLogMessage vaultError =
+  let
+    description = case vaultError of
+      (SecretNotFound secret) ->
+        "Secret not found: " <> sPath secret
+      (KeyNotFound secret) ->
+        "Key " <> (sKey secret) <> " not found for path " <> (sPath secret)
+      (BadRequest resp) ->
+        "Made a bad request: " <> (LBS.unpack resp)
+      (Forbidden) ->
+        "Invalid Vault token"
+      (ServerError resp) ->
+        "Internal Vault error: " <> (LBS.unpack resp)
+      (ServerUnavailable resp) ->
+        "Vault is unavailable for requests. It can be sealed, " <>
+        "under maintenance or enduring heavy load: " <> (LBS.unpack resp)
+      (ServerUnreachable) ->
+        "Network trouble. Host can be unreachable, requests may be " <>
+        "timing out or DNS not working."
+      (Unspecified status resp) ->
+        "Received an error that I don't know about (" <> show status
+        <> "): " <> (LBS.unpack resp)
+  in
+    "[ERROR] " <> description
+
+
+-- Converts a secret name into the name of the environment variable that it
+-- will be available under.
 varNameFromKey :: String -> String -> String
 varNameFromKey path key = fmap format (path ++ "_" ++ key)
   where underscore '/' = '_'
@@ -194,10 +271,16 @@ varNameFromKey path key = fmap format (path ++ "_" ++ key)
         underscore c   = c
         format         = toUpper . underscore
 
---
--- Utilitiy functions
---
 
 maybeToEither :: e -> Maybe a -> Either e a
 maybeToEither _ (Just a) = Right a
 maybeToEither e Nothing  = Left e
+
+
+-- Like splitAt, but also removes the character at the split position.
+cutAt :: Int -> [a] -> ([a], [a])
+cutAt index xs =
+  let
+    (first, second) = splitAt index xs
+  in
+    (first, drop 1 second)

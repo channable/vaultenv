296e87878ff230153a3d3da0382b030579f89890
Laurens Duijvesteijn <laurens@channable.com>
2018-05-11 11:16:22 +0200

    Get packaging and caching working
    I ran into the following:
    
     - We need to build Debian packages outside of the chroot.
     - We need to enable the version check again
     - Enable caching of `.stack-work` and `.stack`

--- build.py
+++ build.py
@@ -11,15 +11,28 @@ from typing import Dict, Optional
 
 import click
 
+from channabuild.config import ConfigLevel
 from channabuild.caching import BuildStep
 from channabuild.exceptions import ChannaBuildUserError
 from channabuild.main import ChannaBuild
 
 cb = ChannaBuild(logging_level=logging.DEBUG)
 
+BINDS = { '.': '/root/build/repo',
+          'cache/stack-work': '/root/build/repo/.stack-work',
+          'cache/stack': '/root/.stack'
+        }
+
+class SetupCache(BuildStep):
+    dependencies = []
+
+    @classmethod
+    def run(cls):
+        cb.ensure_directories(['cache/stack', 'cache/stack-work'])
+
 
 class DownloadRootFs(BuildStep):
-    dependencies = []
+    dependencies = [SetupCache]
 
     @classmethod
     def run(cls):
@@ -65,7 +78,7 @@ class Build(BuildStep):
     dependencies = [SetupRootFs]
 
     @classmethod
-    @cb.execute_in_chroot('chroot', binds={'.': '/root/build/repo'},
+    @cb.execute_in_chroot('chroot', binds=BINDS,
             working_dir='/root/build/repo', environment_overrides={'HOME':'/root'}
             )
     def run(cls):
@@ -77,18 +90,42 @@ class Test(BuildStep):
     dependencies = [Build]
 
     @classmethod
-    @cb.execute_in_chroot('chroot', binds={'.': '/root/build/repo'}, working_dir='/root/build/repo')
+    @cb.execute_in_chroot('chroot', binds=BINDS, working_dir='/root/build/repo',
+        environment_overrides={'HOME':'/root'})
     def run(cls):
         cb.execute(['./integration-test.sh'])
 
 
-class Package(BuildStep):
-    dependencies = [Test]
+class InitializeVersion(BuildStep):
+    dependencies = [SetupRootFs]
+    always_run = True
 
     @classmethod
-    @cb.execute_in_chroot('chroot', binds={'.': '/root/build/repo'}, working_dir='/root/build/repo')
+    def run(cls):
+        repository_config = cb.get_config_layer(ConfigLevel.repository)
+
+        # Figure out the version.
+        stack_version = cb.execute_capture(
+            ['stack', 'query', 'locals', 'vaultenv', 'version']).strip().decode('utf-8')
+        stack_version = stack_version.strip("'")
+
+        git_version = cb.get_tag()
+
+        if stack_version == git_version:
+            repository_config.VERSION = stack_version
+        else:
+            repository_config.VERSION = f'{stack_version}.dev'
+
+
+class BuildPackageTree(BuildStep):
+    dependencies = [Test, InitializeVersion]
+
+    @classmethod
+    @cb.execute_in_chroot('chroot', binds=BINDS,
+            environment_overrides={'HOME':'/root'},working_dir='/root/build/repo')
     def run(cls):
         config = cb.get_config()
+        print(config.VERSION)
 
         # Install vaultenv into ./pkg
         cb.execute(['rm', '-rf', 'pkg/'])
@@ -110,10 +147,16 @@ class Package(BuildStep):
                     netbase
             """))
 
-        stack_path = execute_capture(['stack', 'path', '--local-install-root']).strip().decode()
+        stack_path = cb.execute_capture(['stack', 'path', '--local-install-root']).strip().decode()
         cb.execute(['cp', f'{stack_path}/bin/vaultenv', 'pkg/usr/bin/'])
 
-        # Package it as deb file
+
+class Package(BuildStep):
+    dependencies = [BuildPackageTree, InitializeVersion]
+
+    @classmethod
+    def run(cls):
+        config = cb.get_config()
         cb.build_deb('pkg', f'vaultenv-{config.VERSION}.deb')
 
 
@@ -135,26 +178,6 @@ class CleanArtifacts(BuildStep):
 
 class CleanCache(BuildStep):
     pass
-
-
-# class InitializeVersion(BuildStep):
-#     dependencies = []
-#
-#     @classmethod
-#     def run(cls):
-#         config: Dict[ConfigLevel, Dict[str, Optional[str]]] = defaultdict(lambda: {})
-#
-#         # Figure out the version.
-#         stack_version = execute_capture(
-#             ['stack', 'query', 'locals', 'vaultenv', 'version']).strip().decode('utf-8')
-#         stack_version = stack_version.strip("'")
-#
-#         git_version = get_tag()
-#
-#         if stack_version == git_version:
-#             config[ConfigLevel.repository]['VERSION'] = stack_version
-#         else:
-#             config[ConfigLevel.repository]['VERSION'] = f'{stack_version}.dev'
 
 
 BUILD_GRAPH = BuildStep.__subclasses__()

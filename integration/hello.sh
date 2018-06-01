b3ea623f65b2d90e727e3d0625909e01fd55c533
Laurens Duijvesteijn <laurens@channable.com>
2018-06-01 10:53:21 +0200

    Write a test for the happy path

--- integration/hello.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "1..1"
-echo "ok 1 - everything wired up"
--- test.sh
+++ test.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-prove $(find integration -type f -iname '*.sh' ! -name '_*')
+export VAULT_TOKEN="integration"
+export VAULT_HOST="localhost"
+export VAULT_PORT="8200"
+export VAULT_ADDR="http://${VAULT_HOST}:${VAULT_PORT}"
+
+# Start the Vault server we're going to use for our integration tests
+vault server -dev -dev-root-token-id=${VAULT_TOKEN} &> /dev/null &
+
+# Seed Vault and some secret files
+export VAULT_SEEDS="$(mktemp)"
+cat > ${VAULT_SEEDS} << EOF
+testing#key
+testing#otherkey
+testing2#foo
+testing2#bar
+EOF
+
+vault write secret/testing key=testing42 otherkey=testing8 &> /dev/null
+vault write secret/testing2 foo=val1 bar=val2 &> /dev/null
+
+prove --verbose $(find integration -type f -iname '*.sh' ! -name '_*')
+
+# Cleanup the vault dev server
+kill %%

9f32e757fd094c970c38a076e4e08e5572611803
Laurens Duijvesteijn <laurens@channable.com>
2018-06-01 17:36:32 +0200

    Document all the parser functions

--- src/SecretsFile.hs
+++ src/SecretsFile.hs
@@ -33,6 +33,11 @@ instance Show SecretFileErr where
 
 data SecretFileVersion = V1 | V2
 
+-- | Run a ReadP parser
+--
+-- Report a syntax error (without additional information) or return a list of
+-- secrets. The way to run a ReadP parser is to use the readP_to_S function and
+-- pass it a string.
 runParser :: MonadError SecretFileErr m => FilePath -> ReadP a -> String -> m a
 runParser fileName parser contents =
   let result = (readP_to_S parser) contents
@@ -40,6 +45,12 @@ runParser fileName parser contents =
     [] -> throwError $ ParseError fileName
     _ -> pure . fst . head $ result
 
+-- | Parser for the entire secret file format
+--
+-- Determines the version of the format, by looking for @VERSION 2@. Default
+-- to V1 if this is absent. The @V1@ format consists of single lines of secret
+-- specifications. The @V2@ format contains multiple "blocks" of secrets. Each
+-- block specifies which mount point it pertains to.
 secretFileP :: ReadP [Secret]
 secretFileP = do
   fileVersion <- option V1 (formatVersionP <* skipSpaces)
@@ -50,11 +61,20 @@ secretFileP = do
 formatVersionP :: ReadP SecretFileVersion
 formatVersionP = const V2 <$> string "VERSION 2"
 
+-- | Parse a secret block
+--
+-- Exclusive to V2 of the format. A secret block consists of a line describing
+-- the mount location followed by secret specifications.
 secretBlockP :: ReadP [Secret]
 secretBlockP = do
   mountPath <- string "MOUNT " *> manyTill get (char '\n')
   many (secretP V2 mountPath)
 
+-- | Parse a secret specification line
+--
+-- The version of the fileformat we're parsing determines the way we report
+-- variable information. For V2, the mount point is part of the variable name,
+-- to allow for disambiguation. For V1, this is not needed.
 secretP :: SecretFileVersion -> String -> ReadP Secret
 secretP version mount = do
   varName <- option Nothing $ Just <$> manyTill get (char '=')
@@ -78,6 +98,9 @@ getVarName version mount path key = fmap format $ intercalate "_" components
           V1 -> [path, key]
           V2 -> [mount, path, key]
 
+-- | Read a list of secrets from @fileName@.
+--
+-- Does minimal error reporting, but does differentiate IO and parse errors.
 readSecretList :: (MonadError SecretFileErr m, MonadIO m) => FilePath -> m [Secret]
 readSecretList fileName = do
   contents <- liftIO $ safeReadFile
@@ -88,4 +111,3 @@ readSecretList fileName = do
     safeReadFile =
       Exception.catch (Just <$> readFile fileName)
         ((\_ -> return Nothing) :: Exception.IOException -> IO (Maybe String))
-

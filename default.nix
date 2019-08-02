073511179c171864b2b589c85ca8e989733e86d9
Ruud van Asseldonk <ruud@channable.com>
2019-08-02 17:03:31 +0200

    Try adding Nix definitions for a static executable

---
+++ default.nix
@@ -0,0 +1,44 @@
+# Run using:
+#
+#     $(nix-build --no-link -A fullBuildScript)
+{
+  stack2nix-output-path ? "custom-stack2nix-output.nix",
+}:
+let
+  cabalPackageName = "vaultenv";
+  compiler = "ghc844";
+
+  # Pin nixpkgs version.
+  pkgs = import (import ./nixpkgs-pinned.nix) {};
+
+  # Pin static-haskell-nix version.
+  static-haskell-nix = fetchTarball https://github.com/nh2/static-haskell-nix/archive/1d37d9a83e570eceef9c7dad5c89557f8179a076.tar.gz;
+
+  stack2nix-script = import "${static-haskell-nix}/static-stack2nix-builder/stack2nix-script.nix" {
+    inherit pkgs;
+    stack-project-dir = toString ./.; # where stack.yaml is
+    hackageSnapshot = "2019-08-02T00:00:00Z"; # pins e.g. extra-deps without hashes or revisions
+  };
+
+  static-stack2nix-builder = import "${static-haskell-nix}/static-stack2nix-builder/default.nix" {
+    normalPkgs = pkgs;
+    inherit cabalPackageName compiler stack2nix-output-path;
+    # disableOptimization = true; # for compile speed
+  };
+
+  # Full invocation, including pinning `nix` version itself.
+  fullBuildScript = pkgs.writeScript "stack2nix-and-build-script.sh" ''
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    STACK2NIX_OUTPUT_PATH=$(${stack2nix-script})
+    ${pkgs.nix}/bin/nix-build --no-link -A static_package --argstr stack2nix-output-path "$STACK2NIX_OUTPUT_PATH" "$@"
+  '';
+
+in
+  {
+    static_package = static-stack2nix-builder.static_package;
+    inherit fullBuildScript;
+    # For debugging:
+    inherit stack2nix-script;
+    inherit static-stack2nix-builder;
+  }
---
+++ nixpkgs-pinned.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/314775040bb5e32101458f3afe93bfc0e0591112.tar.gz";
+  sha256 = "1scq233g7vcw6v6axc1ga19a0q1b6bj8qy76pmc76d2bafhrvjrn";
+}

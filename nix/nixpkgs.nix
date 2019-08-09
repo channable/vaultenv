5d558b7f08580c195fc8c376d44568946a728b0b
Laurens Duijvesteijn <laurens@channable.com>
2019-08-09 16:01:17 +0200

    nix: Use nixpkgs from static-haskell-nix

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,8 @@
 let
-  nixpkgsRev = "7a53b1cbe79d91167874eb4f0d4ccc69c2e45007";
+  nixpkgsRev = "a2d7e9b875e8ba7fd15b989cf2d80be4e183dc72";
   nixpkgsTar = fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-      sha256 = "sha256:1mwxipvyrwfbl0z6mghv0gr6z61awidnpld1p8h4fwjfifv0dn2j";
+      url = "https://github.com/nh2/nixpkgs/archive/${nixpkgsRev}.tar.gz";
+      sha256 = "sha256:1hnmp637r99qd6g0sbx4w3za564gbzwl5c4z0x7fvn7kfi2jp1hx";
     };
   nixpkgsConfig = {
     packageOverrides = pkgsOld: {

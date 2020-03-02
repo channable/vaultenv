5e35bbf7acf51254f4b559ae81350cf8190100af
Maarten van den Berg <maarten@channable.com>
2020-03-02 17:21:05 +0100

    package: Move .deb manipulation into fakeroot, add postinst script

--- package/build_package.sh
+++ package/build_package.sh
@@ -18,24 +18,41 @@ export VERSION=$(stack query locals vaultenv-real version | sed -e "s/^'//" -e "
 # The name of the .deb file to create
 PKGNAME="vaultenv-${VERSION}"
 
-# Recreate the file system layout as it should be on the target machine.
-mkdir -p "$PKGNAME/DEBIAN"
-mkdir -p "$PKGNAME/usr/bin"
-mkdir -p "$PKGNAME/etc/secrets.d"
-
 cd ..
 VAULTENV_NIX_PATH=$($(nix-build --no-link -A full-build-script nix/vaultenv-static.nix) | tail -n1)
 cd -
 
-cp "${VAULTENV_NIX_PATH}/bin/vaultenv" "$PKGNAME/usr/bin/"
-cp "${VAULTENV_NIX_PATH}/bin/vaultenv" "vaultenv-${VERSION}-linux-musl"
+cp --no-preserve=mode,ownership "${VAULTENV_NIX_PATH}/bin/vaultenv" "vaultenv-${VERSION}-linux-musl"
 
-# Write the package metadata file, substituting environment variables in the
-# template file.
-cat deb_control | envsubst > "$PKGNAME/DEBIAN/control"
 
 # Build the Debian package
-dpkg-deb --build "$PKGNAME"
+# We need to use `fakeroot` here to have the package files be correctly owned
+# by `root` without being `root` ourselves.
+fakeroot -- bash <<-EOFAKEROOT
+  # Re-enable safe scripting options since this is a new shell
+  set -euf -o pipefail
+
+  # Ensure created directories are mode 0755 (default is 0775)
+  umask 022
+
+  # Recreate the file system layout as it should be on the target machine.
+  mkdir -p "$PKGNAME/DEBIAN"
+  mkdir -p "$PKGNAME/usr/bin"
+  mkdir -p "$PKGNAME/etc/secrets.d"
+
+  # Write the package metadata file, substituting environment variables in the
+  # template file.
+  cat deb_control | envsubst > "$PKGNAME/DEBIAN/control"
+
+  cp deb_postinst "$PKGNAME/DEBIAN/postinst"
+  chmod 0755 "$PKGNAME/DEBIAN/postinst"
+
+  # Copy the built binary from the Nix store to the target directory
+  cp --no-preserve=mode,ownership "${VAULTENV_NIX_PATH}/bin/vaultenv" "$PKGNAME/usr/bin/"
+  chown root:root "$PKGNAME/usr/bin/vaultenv"
+  chmod 0755 "$PKGNAME/usr/bin/vaultenv"
+  dpkg-deb --build "$PKGNAME"
+EOFAKEROOT
 
 # Clean up temporary files
 rm -fr "$PKGNAME"

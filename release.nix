73d78098be1474691db96a33b9b6a146eb9bd557
Laurens Duijvesteijn <laurens@channable.com>
2020-04-15 09:05:45 +0200

    Make release.nix a recursive attrset
    This makes it easier to play with everything that is in scope using
    the Nix REPL.

--- release.nix
+++ release.nix
@@ -1,5 +1,5 @@
 # This file wires up the different vaultenv releases that are available.
-let
+rec {
   pkgs = import ./nix/nixpkgs.nix {};
 
   static = import ./nix/static-haskell-nix.nix {
@@ -47,43 +47,42 @@ let
         mv vaultenv.deb "$out/vaultenv-${version}.deb"
       '';
     };
-in
-  rec {
-    # Normal cabal build where Nix handles dependencies.
-    vaultenv = pkgs.haskellPackages.callPackage ./nix/vaultenv.nix {};
 
-    # NB: For both of these packages, we pass arguments explicitly instead
-    # of using `callPackage`. `callPackage` from `static-haskell-nix` does not
-    # appear to work.
+  # Normal cabal build where Nix handles dependencies.
+  vaultenv = pkgs.haskellPackages.callPackage ./nix/vaultenv.nix {};
 
-    # There is still something wrong with this derivation. I get errors in
-    # the tests of the TLS library.
-    vaultenv-static = (import ./nix/vaultenv.nix {
-      haskellPackages = static.haskellPackages;
-      static = true;
-      lib = static.lib;
-      pkgs = static.pkgs;
-      mkDerivation = static.haskellPackages.mkDerivation;
-      Cabal = static.haskellPackages.Cabal;
-    });
+  # NB: For both of these packages, we pass arguments explicitly instead
+  # of using `callPackage`. `callPackage` from `static-haskell-nix` does not
+  # appear to work.
 
-    # Static binary including GMP. We cannot distribute this because vaultenv is
-    # not LGPL licensed. We can build it for ourselves though. We want this,
-    # because `integer-gmp` is a lot faster to use for TLS connections than
-    # `integer-simple`. This is useful when you start vaultenv a lot. Most users
-    # shouldn't need this.
-    vaultenv-static-gmp = (import ./nix/vaultenv.nix {
-      haskellPackages = static-gmp.haskellPackages;
-      static = true;
-      lib = static-gmp.lib;
-      pkgs = static-gmp.pkgs;
-      mkDerivation = static-gmp.haskellPackages.mkDerivation;
+  # There is still something wrong with this derivation. I get errors in
+  # the tests of the TLS library.
+  vaultenv-static = (import ./nix/vaultenv.nix {
+    haskellPackages = static.haskellPackages;
+    static = true;
+    lib = static.lib;
+    pkgs = static.pkgs;
+    mkDerivation = static.haskellPackages.mkDerivation;
+    Cabal = static.haskellPackages.Cabal;
+  });
 
-      # We need to explicitly pass the Cabal version from static-haskell-nix
-      # because it includes some relevant patches.
-      Cabal = static-gmp.haskellPackages.Cabal;
-    });
+  # Static binary including GMP. We cannot distribute this because vaultenv is
+  # not LGPL licensed. We can build it for ourselves though. We want this,
+  # because `integer-gmp` is a lot faster to use for TLS connections than
+  # `integer-simple`. This is useful when you start vaultenv a lot. Most users
+  # shouldn't need this.
+  vaultenv-static-gmp = (import ./nix/vaultenv.nix {
+    haskellPackages = static-gmp.haskellPackages;
+    static = true;
+    lib = static-gmp.lib;
+    pkgs = static-gmp.pkgs;
+    mkDerivation = static-gmp.haskellPackages.mkDerivation;
 
-    vaultenv-static-deb = mkDeb "0.13.1" vaultenv-static;
-    vaultenv-static-gmp-deb = mkDeb "0.13.1" vaultenv-static-gmp;
-  }
+    # We need to explicitly pass the Cabal version from static-haskell-nix
+    # because it includes some relevant patches.
+    Cabal = static-gmp.haskellPackages.Cabal;
+  });
+
+  vaultenv-static-deb = mkDeb "0.13.1" vaultenv-static;
+  vaultenv-static-gmp-deb = mkDeb "0.13.1" vaultenv-static-gmp;
+}

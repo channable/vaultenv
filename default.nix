afb761160dfe3ea9f2ab49beafd61484a93af2ea
Ruud van Asseldonk <ruud@chorus.one>
2021-10-07 14:46:02 +0200

    Add glibcLocales to development environment

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@
     name = "vaultenv-devenv";
     paths = [
       cachix
+      glibcLocales  # So you can export LOCALE_ARCHIVE=$(nix path-info)/lib/locale/locale-archive.
       niv
       perl # For "prove"
       python3

8498cfb01c8088fba196f2ba7eb6c6411b1ed12d
Ruud van Asseldonk <ruud@chorus.one>
2024-02-22 15:24:55 +0100

    Add regression test for secrets file with comments
    This test currently fails because Vaultenv drops the second secret. I
    need to fix that, but let's add the test first.

--- test/SecretFileSpec.hs
+++ test/SecretFileSpec.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 
 import Data.Either (isRight, isLeft)
 
+
 import qualified SecretsFile
 import qualified System.Directory as Dir
 
@@ -21,3 +22,22 @@ spec = do
       let invalidTestFiles = map ("test/invalid/" <>) invalidTestContents
       parseResults <- mapM SecretsFile.readSecretsFile invalidTestFiles
       parseResults `shouldSatisfy` (all isLeft)
+
+    it "parses all v2-comments.secrets as expected" $ do
+      -- This is a regression test, Vaultenv 0.16.0 used to drop the second
+      -- secret.
+      Right parseResults <- SecretsFile.readSecretsFile "test/golden/v2-comments.secrets"
+      parseResults `shouldBe`
+        [ SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "devices/frobnicator"
+          , SecretsFile.sKey = "PASSWORD"
+          , SecretsFile.sVarName = "FROBNICATOR_PASSWORD"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "devices/widgets/turboencabulator"
+          , SecretsFile.sKey = "PIN"
+          , SecretsFile.sVarName = "WIDGET_PIN"
+          }
+        ]

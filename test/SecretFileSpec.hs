8a51b4da7ff0c6d82ec520d37c5e5abf29ebbe2d
Ruud van Asseldonk <ruud@chorus.one>
2024-02-22 18:03:31 +0100

    Add more tests for the secrets file parser
    I want to make sure that touching the parser doesn't break things.

--- test/SecretFileSpec.hs
+++ test/SecretFileSpec.hs
@@ -25,7 +25,83 @@ spec = do
         parseResult <- SecretsFile.readSecretsFile fname
         parseResult `shouldSatisfy` isLeft
 
-    it "parses all v2-comments.secrets as expected" $ do
+    it "parses v1.secrets as expected" $ do
+      Right parseResults <- SecretsFile.readSecretsFile "test/golden/v1.secrets"
+      parseResults `shouldBe`
+        [ SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo"
+          , SecretsFile.sKey = "bar"
+          , SecretsFile.sVarName = "FOO_BAR"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo/bar"
+          , SecretsFile.sKey = "baz"
+          , SecretsFile.sVarName = "FOO_BAR_BAZ"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "bar"
+          , SecretsFile.sKey = "baz"
+          , SecretsFile.sVarName = "FOO"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo/baz"
+          , SecretsFile.sKey = "quix"
+          , SecretsFile.sVarName = "BAR"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo/single"
+          , SecretsFile.sKey = "underscore"
+          , SecretsFile.sVarName = "single_underscore"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo/double"
+          , SecretsFile.sKey = "underscore"
+          , SecretsFile.sVarName = "double__underscore"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo/double"
+          , SecretsFile.sKey = "underscore"
+          , SecretsFile.sVarName = "_leading_underscore"
+          }
+        ]
+
+    it "parses v2.secrets as expected" $ do
+      Right parseResults <- SecretsFile.readSecretsFile "test/golden/v2.secrets"
+      parseResults `shouldBe`
+        [ SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo"
+          , SecretsFile.sKey = "bar"
+          , SecretsFile.sVarName = "SECRET_FOO_BAR"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "secret"
+          , SecretsFile.sPath = "foo/baz"
+          , SecretsFile.sKey = "bar"
+          , SecretsFile.sVarName = "BAR"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "otherthing"
+          , SecretsFile.sPath = "foo"
+          , SecretsFile.sKey = "bar"
+          , SecretsFile.sVarName = "OTHERTHING_FOO_BAR"
+          }
+        , SecretsFile.Secret
+          { SecretsFile.sMount = "otherthing"
+          , SecretsFile.sPath = "foo/baz"
+          , SecretsFile.sKey = "bar"
+          , SecretsFile.sVarName = "BAR"
+          }
+        ]
+
+    it "parses v2-comments.secrets as expected" $ do
       -- This is a regression test, Vaultenv 0.16.0 used to drop the second
       -- secret.
       Right parseResults <- SecretsFile.readSecretsFile "test/golden/v2-comments.secrets"
